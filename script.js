8011e3dc4c96d8a0c06a460fcbce35c9d8f13ff2
projetointegrador0 <150063672+projetointegrador0@users.noreply.github.com>
2023-11-06 15:12:46 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,73 @@
+function showMoreum() {
+    var text = document.getElementById("textum");
+    var buttonum = document.querySelector("button");
+
+    if (text.style.maxHeight === "60px") {
+        text.style.maxHeight = "none";
+        buttonum.innerHTML = "Leia Menos";
+    } else {
+        text.style.maxHeight = "60px";
+        buttonum.innerHTML = "Leia Mais";
+    }
+}
+
+
+function showMoredois() {
+    var text = document.getElementById("textdois");
+    var button = document.querySelector("buttondois");
+    
+    if (text.style.maxHeight === "60px") {
+        text.style.maxHeight = "none";
+        buttondois.innerHTML = "Leia Menos";
+    } else {
+        text.style.maxHeight = "60px";
+        buttondois.innerHTML = "Leia Mais";
+    }
+}
+
+function showMoretres() {
+    var text = document.getElementById("texttres");
+    var button = document.querySelector("buttontres");
+    
+    if (text.style.maxHeight === "60px") {
+        text.style.maxHeight = "none";
+        buttontres.innerHTML = "Leia Menos";
+    } else {
+        text.style.maxHeight = "60px";
+        buttontres.innerHTML = "Leia Mais";
+    }
+}
+
+function showMorequatro() {
+    var text = document.getElementById("textquatro");
+    var button = document.querySelector("buttonquatro");
+    
+    if (text.style.maxHeight === "60px") {
+        text.style.maxHeight = "none";
+        buttonquatro.innerHTML = "Leia Menos";
+    } else {
+        text.style.maxHeight = "60px";
+        buttonquatro.innerHTML = "Leia Mais";
+    }
+}
+
+function showMorecinco() {
+    var text = document.getElementById("textcinco");
+    var button = document.querySelector("buttoncinco");
+    
+    if (text.style.maxHeight === "60px") {
+        text.style.maxHeight = "none";
+        buttoncinco.innerHTML = "Leia Menos";
+    } else {
+        text.style.maxHeight = "60px";
+        buttoncinco.innerHTML = "Leia Mais";
+    }
+}
+
+
+
+
+
+
+
+
